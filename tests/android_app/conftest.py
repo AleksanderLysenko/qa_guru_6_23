8e302821941320c98981095f5d02dccfd357c45e
Alexandr Lysenko <ovie48@mail.ru>
2023-10-07 15:41:38 +0300

    add attach

--- tests/android_app/conftest.py
+++ tests/android_app/conftest.py
@@ -1,8 +1,10 @@
+import allure
 import pytest
 from appium.options.android import UiAutomator2Options
 from appium import webdriver
 from selene import browser
 import os
+from utils import attach
 
 import project
 
@@ -11,28 +13,36 @@ import project
 def mobile_management():
     options = UiAutomator2Options().load_capabilities({
         # Specify device and os_version for testing
-        "platformVersion": "9.0",
-        "deviceName": "Huawei P30",
+        'platformVersion': '9.0',
+        'deviceName': 'Huawei P30',
 
         # Set URL of the application under test
-        "app": "bs://sample.app",
+        'app': 'bs://sample.app',
 
         # Set other BrowserStack capabilities
         'bstack:options': {
-            "projectName": "qa_guru_6_23",
-            "buildName": "browserstack-build-1",
-            "sessionName": "BStack_6_23",
+            'projectName': 'qa_guru_6_23',
+            'buildName': 'browserstack-build-1',
+            'sessionName': 'BStack_6_23',
 
             # Set your access credentials
-            "userName": project.config.browserstack_username,
-            "accessKey": project.config.browserstack_accesskey,
+            'userName': project.config.browserstack_username,
+            'accessKey': project.config.browserstack_accesskey,
         }
     })
 
-    browser.config.driver = webdriver.Remote(project.config.remote_url, options=options)
+    with allure.step('init app session'):
+        browser.config.driver = webdriver.Remote(project.config.remote_url, options=options)
 
     browser.config.timeout = float(os.getenv('timeout', '10.0'))
 
     yield
 
-    browser.quit()
+    session_id = browser.driver.session_id
+
+    attach.allure_attach_browserstack_screenshot()
+    attach.allure_attach_browserstack_xml_dump()
+    attach.attach_browserstack_video(session_id)
+
+    with allure.step('tear down app session'):
+        browser.quit()
--- tests/android_app/test_wikipedia.py
+++ tests/android_app/test_wikipedia.py
@@ -1,15 +1,15 @@
-from allure_commons._allure import step
+import allure
 from appium.webdriver.common.appiumby import AppiumBy
 from selene import browser, have
 
 
 def test_search(mobile_management):
 
-    with step('Type search'):
+    with allure.step('Type search'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
         browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Appium')
 
-    with step('Verify content found'):
+    with allure.step('Verify content found'):
         results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
         results.should(have.size_greater_than(0))
         results.first.should(have.text('Appium'))
@@ -17,11 +17,11 @@ def test_search(mobile_management):
 
 def test_no_search(mobile_management):
 
-    with step('Type search'):
+    with allure.step('Type search'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
         browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('ewewewewewewew')
 
-    with step('Verify content no found'):
+    with allure.step('Verify content no found'):
         results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
         results.should(have.no.size_greater_than(0))
         results.first.should(have.no.text('ewewewewewewew'))
@@ -29,17 +29,17 @@ def test_no_search(mobile_management):
 
 def test_open_article(mobile_management):
 
-    with step('Type search'):
+    with allure.step('Type search'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
         browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Impressionism')
 
-    with step('Verify content found'):
+    with allure.step('Verify content found'):
         results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
         results.should(have.size_greater_than(0))
         results.first.should(have.text('Impressionism'))
 
-    with step('Open article'):
+    with allure.step('Open article'):
         results.element_by(have.text('Impressionism')).click()
-        with step('Error occured'):
+        with allure.step('Error occured'):
             error = browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/view_wiki_error_text'))
             error.should(have.text("An error occurred"))
